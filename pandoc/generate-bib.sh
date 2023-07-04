5f6dde9da6125e92e97ffd688ccaa154cf852bfc
Rita Laezza <rita.laezza@gmail.com>
2023-07-04 14:20:55 +0200

    Added Kilian's ICML paper.

--- pandoc/generate-bib.sh
+++ pandoc/generate-bib.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# This program uses pandoc to automatically generate the publications markdown
+# This program uses pandoc to automatically generate the publications html
 
 # Specify a set of default option settings: -d FILE, --defaults=FILE
 # FILE is a YAML file whose fields correspond to command-line option settings.
@@ -9,4 +9,4 @@ pandoc citeproc.md -d options.yml --metadata lang="en" --metadata title="List of
 # Manually add header for jekyll:
 cat header.md publications.html >| ../publications.html
 
-echo "If you've added a new paper, don't forget to add the new id to the correct txt file under js/filter/ !!!"
+echo "=> If you've added a new paper, don't forget to add the new id to the correct txt file under js/filter/ !!!"
